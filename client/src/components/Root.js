d539351e100dd7d83341f676c82a1d234375139b
pankajboruah <pankajboruah28.pb@gmail.com>
2021-05-30 21:48:10 +0530

    base setup client complete

---
+++ client/src/components/Root.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import { hot } from "react-hot-loader";
+
+const Root = () => {
+	return (
+		<div className="Root" id="root">
+			<h1> Successful </h1>
+		</div>
+	);
+};
+
+// export default hot(module)(Root);
+export default Root;
